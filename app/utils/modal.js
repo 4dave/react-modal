0e8909ec8163a183b98cdc8186fbbe4b5d9c2b26
David Fredkove <dave.fredkove@gmail.com>
2022-12-08 17:11:05 -0600

    tw to vanilla css

--- app/utils/modal.js
+++ app/utils/modal.js
@@ -1,3 +1,5 @@
+import "./modal.css"
+
 export default function Modal({ isVisible, onClose, children }) {
   if (!isVisible) return null
 
@@ -8,20 +10,23 @@ export default function Modal({ isVisible, onClose, children }) {
   }
 
   return (
-    <div
-      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
-      id="wrapper"
-      onClick={handleClose}
-    >
-      {/* <div className="sm:w-[600px] w-[90%] flex flex-col"> */}
-      <div className="px-2 sm:w-[600px] w-full flex flex-col">
-        <button
+    //   <div
+    //   className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
+    //   id="wrapper"
+    //   onClick={handleClose}
+    // >
+    <div className="modal-wrapper" id="wrapper" onClick={handleClose}>
+      {/* <div className="px-2 sm:w-[600px] w-full flex flex-col"> */}
+      <div className="modal">
+        {/* <button
           className="text-white text-xl place-self-end"
           onClick={() => onClose()}
-        >
+        > */}
+        <button className="modal-close-button" onClick={() => onClose()}>
           X
         </button>
-        <div className="bg-white p-2 rounded">{children}</div>
+        {/* <div className="bg-white p-2 rounded">{children}</div> */}
+        <div className="modal-body">{children}</div>
       </div>
     </div>
   )
